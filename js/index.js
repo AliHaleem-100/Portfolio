51abe4e821863d5cbcc1d9054c75b0bbccd3a47b
ali <user@gmail.com>
2024-04-05 11:47:53 +0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
+// variables
 let navbar = document.querySelector(".navbar");
 let barIcon = document.querySelector(".bar-icon");
 let banner_circle = document.querySelector(".banner-circle");
+
 barIcon.addEventListener("click", () => {
   barIcon.classList.toggle("cross");
   banner_circle.classList.toggle("show");
   navbar.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 306)
+    banner_circle.classList.add("scroll");
+  else banner_circle.classList.remove("scroll");
 });
